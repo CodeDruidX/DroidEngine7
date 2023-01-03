60c4927ab4c2a271c9b4a244d43a9f4fefcadc5e
CodeDroidX <52743561+CodeDroidX@users.noreply.github.com>
2023-01-04 02:11:19 +0700

    Оно кое-как заработало!

--- DroidEngine7.py
+++ DroidEngine7.py
@@ -5,7 +5,7 @@ print(f"Droid 3d Engine v{v}")
 import numpy as np
 import cv2
 import pygame
-
+import math
 
 def img_into_polygon(np_img: np.float32, np_polygon: np.float32):
     h, w, _ = np_img.shape
@@ -24,36 +24,64 @@ def img_into_polygon(np_img: np.float32, np_polygon: np.float32):
     ).convert_alpha()  # Конвертация в pygame пригодный формат
 
 
+
+def unit_vector(vector): return vector / np.linalg.norm(vector)
+
+def angle_between(v1, v2):
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.degrees(np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0)))
+
 def view3d(camera: np.float32, point: np.float32):
     camera_system = point - camera[0]
 
-    xy_d = np.linalg.norm(camera_system[:2])
+    """
+    xy_d = math.copysign(np.linalg.norm(camera_system[:2]),camera_system[0]*camera_system[1])
     xyz_d = np.arctan2(xy_d, camera_system[2]) - np.radians(camera[1][0])
 
-    zy_d = np.linalg.norm(camera_system[1:])
+    zy_d = math.copysign(np.linalg.norm(camera_system[1:]),camera_system[1]*camera_system[2])
     zyx_d = np.arctan2(zy_d, camera_system[0]) - np.radians(camera[1][1])
+    """
+    #print(camera_system)
+    vert=math.copysign(angle_between(camera_system,np.array([camera_system[0],camera_system[1],0.])),camera_system[2])
+    bhor=math.copysign(angle_between(camera_system,np.array([camera_system[0],0.,camera_system[2]])),1)
+    #print(hor,vert)
+    B=math.copysign(angle_between(camera_system,np.array([0,0.,camera_system[2]])),1)
 
-    return np.linalg.norm(camera_system), np.array([zyx_d, xyz_d])
+
+    if camera_system[0]>=0:
+        if camera_system[1]>=0:
+            hor=bhor
+        else:
+            hor=-bhor
+    else:
+        if camera_system[1]>=0:
+            hor=2*B-bhor
+        else:
+            hor=-2*B+bhor
+
+    print(bhor,"->",hor,camera_system,B)
+
+    return np.float32([hor,vert]) - camera[1]
 
 
 def screen_transform(angles, w, h, fov):
-    dir1, dir2 = angles
-    w = ((dir1 - 90) / fov) * w
-    h = ((dir2 - 90) / fov) * h
+    dir1, dir2 = angles/fov
+    w,h = w/2-dir1 * w, h/2-dir2 * h
 
     return int(w), int(h)
 
 
 def render(camera, point, w=1920, h=1080, fov=90):
-    d, angles = view3d(camera, point)
-    return d, screen_transform(angles, w, h, fov)
+    angles = view3d(camera, point)
+    return screen_transform(angles, w, h, fov),angles
 
 
 # camera=[[x,y],[vert_angle,horis_angle]]
-camera = np.float32([[0, 0, 0], [0, 0, 0]])
+camera = np.array([[0., 0., 0.], [0., 0.]])
 
 
-"""
+
 
 from math import hypot,sin,cos,atan,degrees,radians
 import pygame
@@ -91,17 +119,13 @@ while True:
     #print(camera[1])
     if True:
             if pressed_keys[pygame.K_w]:
-                camera[0][1]+=cos(radians(camera[1][0]+180))*0.2
-                camera[0][0]+=sin(radians(camera[1][0]+180))*0.2
+                camera[0][0]+=0.2
             elif pressed_keys[pygame.K_s]:
-                camera[0][1]+=cos(radians(camera[1][0]))*0.2
-                camera[0][0]+=sin(radians(camera[1][0]))*0.2
+                camera[0][0]-=0.2
             if pressed_keys[pygame.K_a]:
-                camera[0][1]+=cos(radians(camera[1][0]+90))*0.2
-                camera[0][0]+=sin(radians(camera[1][0]+90))*0.2
+                camera[0][1]+=0.2
             elif pressed_keys[pygame.K_d]:
-                camera[0][1]+=cos(radians(camera[1][0]-90))*0.2
-                camera[0][0]+=sin(radians(camera[1][0]-90))*0.2
+                camera[0][1]-=0.2
             if pressed_keys[pygame.K_r]:
                 camera[0][2]+=0.2
             elif pressed_keys[pygame.K_f]:
@@ -112,16 +136,26 @@ while True:
     x-=WIDTH/2
     y-=HEIGHT/2
     pyautogui.moveTo(WIDTH/2, HEIGHT/2)
-    camera[1][0]+=x
-    camera[1][1]+=y
-
+    camera[1][0]-=x
+    camera[1][1]-=y
+    if camera[1][0]>180:camera[1][0]-=360
+    if camera[1][0]<-180:camera[1][0]+=360
+    if camera[1][1]>180:camera[1][1]-=360
+    if camera[1][1]<-180:camera[1][1]+=360
     screen.fill(BLACK)
-    d,pos=render(camera,np.float32([1,1,1]))
-    x,y=960-pos[0],540+pos[1]
-    print(pos,x,y)
-    pygame.draw.circle(screen, WHITE, (x,y), 100/d)
+    
+    pos,a=render(camera,np.float32([10,0,0.9]))
+    pygame.draw.circle(screen, WHITE, pos, 10)
+
+    pos,a=render(camera,np.float32([10,0.9,0.9]))
+    pygame.draw.circle(screen, (0,0,255), pos, 10)
+
+    pos,a=render(camera,np.float32([-10,0,0.9]))
+    pygame.draw.circle(screen, WHITE, pos, 10)
+    
+    pos,a=render(camera,np.float32([-10,0.9,0.9]))
+    pygame.draw.circle(screen, (255,0,0), pos, 10)
     pygame.display.update() 
 
 
 
-"""
